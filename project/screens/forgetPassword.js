264247f56cf1d8f9c9b624faf081f80962bfc5df
ميرنا الشريف <merna.m.elsherief@gmail.com>
2023-04-03 19:21:00 +0200

    first

---
+++ project/screens/forgetPassword.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const forgetPassword = () => {
+  return (
+    <View>
+      <Text>forgetPassword</Text>
+    </View>
+  );
+};
+
+export default forgetPassword;
