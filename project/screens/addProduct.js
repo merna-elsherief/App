95b99fee019588e30254acb4ffee74c223cedb13
ميرنا الشريف <merna.m.elsherief@gmail.com>
2023-05-07 15:08:29 +0300

    modify

--- project/screens/addProduct.js
+++ project/screens/addProduct.js
@@ -44,7 +44,7 @@ const addProduct = ({ navigation }) => {
     setProductInfo({ ...productInfo, [fieldName]: value });
   };
   const addProductToDataBase = async () => {
-    const docRef = await addDoc(collection(db, "products"), {
+    const docRef2 = await addDoc(collection(db, "products"), {
       title: title,
       desc: desc,
       price: price,
@@ -52,8 +52,9 @@ const addProduct = ({ navigation }) => {
       img: img,
       instock: count === 0 ? false : true,
     });
+      console.log("Document written with ID: ", docRef2.id);
   };
-  console.log("Document written with ID: ", docRef.id);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Add Books</Text>
